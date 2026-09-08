e3cf04b51fdc265491e562f51a71a5d1b9c5b93d
Barak <Barakc121@gmail.com>
2026-09-09 00:03:02 +0300

    Use Nahariya price list in Kfar Yabetz project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const STORAGE_KEY = "arbaat-haminim-orders-v2";
 const GOOGLE_SHEET_CSV_URL =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSKXTurywqKCwdpag0pR4sg3WLISAptW8M6CB0HIhXhjIXyWycbzC7onXOBXwfmUvWOKwIBquSkY9L7/pub?output=csv&gid=0";
 const ORDERS_API_URL = "https://script.google.com/macros/s/AKfycbyvyYsBneX50VaKTu7a09srH7uYPa6OVn1FjhFe4n1TiZ0F2JrjrSFRST_10bT9srNDgQ/exec";
-const ADMIN_PASSWORD = "033568221";
+const ADMIN_PASSWORD = "325276319";
 const state = {
   orders: [],
 };
@@ -17,6 +17,15 @@ const defaultProducts = [
   "כשר לברכה",
   "כשר לברכה מרוקאי",
   "כשר לברכה תימני",
+  "מהודר תימני",
+  "מהודר א תימני",
+  "מהודר א א תימני",
+  "מהודר מרוקאי",
+  "מהודר א מרוקאי",
+  "מהודר א א מרוקאי",
+  "הדסי רש\"י",
+  "נרתיק מהודר לולב",
+  "קופסא מהודרת לאתרוג",
 ];
 
 const form = document.getElementById("orderForm");
